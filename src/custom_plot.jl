9907b7c4e8fca954b7575b833c7076bb530c4cc9
AmeGit01 <amedeo.repele@unitn.it>
2026-09-18 16:01:50 +0200

    custom_plot.jl apposto

--- src/custom_plot.jl
+++ src/custom_plot.jl
@@ -30,71 +30,79 @@ function plot_df!(plt, x, y, label::AbstractString)
     return
 end
 
+const InputFolder = "outputs"
+const OutputFolder = joinpath("outputs", "figures")
+
 function main_custom()
-    InputFile = "Discharge_IC_results.csv"
-    InputPath = joinpath("csv_files", InputFile)
+	InputPath = joinpath(InputFolder, "*discharge.csv")
+    InputFiles = glob(InputPath)
+	println("Found $(length(InputFiles)) CSV files:", InputFiles)
 
-	# Read data
-    println("Reading ", InputPath, "...")
-    df = CSV.read(InputPath, DataFrame)
+    # InputFile = "Discharge_IC_results.csv"
+    # InputPath = joinpath("csv_files", InputFile)
 
-	# Print columns
-	println("Columns: ")
-	Cols = names(df)
-	foreach(println, enumerate(Cols))
+	for File ∈ InputFiles
+		# Read data
+	    println("Reading ", File, "...")
+	    df = CSV.read(File, DataFrame)
 
-	# Select columns
-	println("Select columns to be plotted (ex. 2 5 6):")
-	input = readline()
+		# Print columns
+		println("Columns: ")
+		Cols = names(df)
+		foreach(println, enumerate(Cols))
 
-	if isempty(input) 
-		println("No column selected, all columns will be plotted!!")
-		Nall = length(Cols)
-		DatasetsToPlot = collect(2:1:Nall)
-		@infiltrate false
-	else
-		DatasetsToPlot = parse.(Int64, split(input)) # [8, 9]
-	end
-
-	# transform seconds to hours
-	xh = df[:, 1] ./ 3600
-
-	# Reverce eventual wrong directioned nodestring
-	reverse_list = [24, 25] # [23, 24]
-	for n ∈ reverse_list
-		df[:, n] .= -df[:, n]
-	end
-
-	# Plot the data
-	FirstPlot = true
-	plt = plot()
-	for n ∈ DatasetsToPlot
-		if FirstPlot			
-			println("Choose the plot title: ")
-			title = readline()
-			plot_df!(plt, xh, df[:, n], Cols[n][begin:end-7], title)
-			FirstPlot = false
-		else
-			plot_df!(plt, xh, df[:, n], Cols[n][begin:end-7])
-		end
-	end
-	plot!(plt, size=(800, 600), left_margin=3mm)
-	display(plt)
-
-	println("Do you want to save the plot? (y/n)")
-	input = readline()
-
-	if input == "y" || input == "Y"
-		println("Digit the file name: ")
+		# Select columns
+		println("Select columns to be plotted (ex. 2 5 6):")
 		input = readline()
 
-		OutputPath = joinpath("images", input)
-		savefig(plt, OutputPath)
+		if isempty(input) 
+			println("No column selected, all columns will be plotted!!")
+			Nall = length(Cols)
+			DatasetsToPlot = collect(2:1:Nall)
+			@infiltrate false
+		else
+			DatasetsToPlot = parse.(Int64, split(input)) # [8, 9]
+		end
 
-		println("Plot saved: ", OutputPath)
+		# transform seconds to hours
+		xh = df[:, 1] ./ 3600
+
+		# Reverce eventual wrong directioned nodestring
+		reverse_list = [] # [23, 24]
+		for n ∈ reverse_list
+			df[:, n] .= -df[:, n]
+		end
+
+		# Plot the data
+		FirstPlot = true
+		plt = plot()
+		for n ∈ DatasetsToPlot
+			if FirstPlot			
+				println("Choose the plot title: ")
+				title = readline()
+				plot_df!(plt, xh, df[:, n], Cols[n][begin:end-7], title)
+				FirstPlot = false
+			else
+				plot_df!(plt, xh, df[:, n], Cols[n][begin:end-7])
+			end
+		end
+		plot!(plt, size=(800, 600), left_margin=3mm)
+		display(plt)
+
+		println("Do you want to save the plot? (y/n)")
+		input = readline()
+
+		if input == "y" || input == "Y"
+			println("Digit the file name: ")
+			input = readline()
+
+			OutputPath = joinpath(OutputFolder, input)
+			savefig(plt, OutputPath)
+			isfile(OutputPath) && println("Plot saved: ", OutputPath)
+		end
+
+		@infiltrate false
 	end
-
-	@infiltrate false
 
     return nothing
 end
@@ -108,4 +116,4 @@ main_custom()
 # 14 15 16 
 
 # Andamento portata
-# 20 21 13 17 7
+# 13 21 22 14 18 7
